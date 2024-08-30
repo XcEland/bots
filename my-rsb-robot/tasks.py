2b442fb3170b5aa86dbc518314bc619065b4c57e
XcEland <mkoma.xyz@gmail.com>
2024-08-30 16:25:58 +0200

    multiple entries

--- my-rsb-robot/tasks.py
+++ my-rsb-robot/tasks.py
@@ -3,6 +3,7 @@ from robocorp import browser
 
 from RPA.HTTP import HTTP
 from RPA.Excel.Files import Files
+from RPA.PDF import PDF
 
 @task
 def robot_spare_bin_python():
@@ -14,6 +15,9 @@ def robot_spare_bin_python():
     download_excel_file()
     # fill_and_submit_sales_form()
     fill_form_with_excel_data()
+    collect_results()
+    # export_as_pdf()
+    log_out()
 
 def open_the_intranet_website():
     browser.goto("https://robotsparebinindustries.com/")
@@ -28,8 +32,9 @@ def fill_and_submit_sales_form(sales_rep):
     page = browser.page()
     page.fill("#firstname", sales_rep["First Name"])
     page.fill("#lastname",sales_rep["Last Name"])
-    page.fill("#salesresult",str(sales_rep["Sales Target"]))
-    page.select_option("#salestarget", str(sales_rep["Sales"]))
+    page.fill("#salesresult", str(sales_rep["Sales"]))
+    page.select_option("#salestarget", str(sales_rep["Sales Target"]))
+    
     page.click("text=Submit")
 
 def download_excel_file():
@@ -44,3 +49,18 @@ def fill_form_with_excel_data():
 
     for row in worksheet:
         fill_and_submit_sales_form(row)
+
+def collect_results():
+    page = browser.page()
+    page.screenshot(path="output/sales_summary.png")
+
+def export_as_pdf():
+    page =browser.page()
+    sales_results_html= page.locator("#sales-results").inner_html()
+
+    pdf = PDF()
+    pdf.html_to_pdf(sales_results_html, "output/sales_results.pdf")
+
+def log_out():
+    page = browser.page()
+    page.click("text=Log out")
