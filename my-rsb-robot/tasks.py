d16719d39c2aba24e73913d3ef9326cabc687d4e
XcEland <mkoma.xyz@gmail.com>
2024-08-30 10:19:48 +0200

    multiple signins

--- my-rsb-robot/tasks.py
+++ my-rsb-robot/tasks.py
@@ -1,6 +1,9 @@
 from robocorp.tasks import task
 from robocorp import browser
 
+from RPA.HTTP import HTTP
+from RPA.Excel.Files import Files
+
 @task
 def robot_spare_bin_python():
     browser.configure(
@@ -8,7 +11,9 @@ def robot_spare_bin_python():
     )
     open_the_intranet_website()
     log_in()
-    fill_and_submit_sales_form()
+    download_excel_file()
+    # fill_and_submit_sales_form()
+    fill_form_with_excel_data()
 
 def open_the_intranet_website():
     browser.goto("https://robotsparebinindustries.com/")
@@ -19,10 +24,23 @@ def log_in():
     page.fill("#password", "thoushallnotpass")
     page.click("button:text('Log in')")
 
-def fill_and_submit_sales_form():
+def fill_and_submit_sales_form(sales_rep):
     page = browser.page()
-    page.fill("#firstname", "John")
-    page.fill("#lastname","Smith")
-    page.fill("#salesresult","123")
-    page.select_option("#salestarget", "10000")
+    page.fill("#firstname", sales_rep["First Name"])
+    page.fill("#lastname",sales_rep["Last Name"])
+    page.fill("#salesresult",str(sales_rep["Sales Target"]))
+    page.select_option("#salestarget", str(sales_rep["Sales"]))
     page.click("text=Submit")
+
+def download_excel_file():
+    http = HTTP()
+    http.download(url="https://robotsparebinindustries.com/SalesData.xlsx", overwrite=True)
+
+def fill_form_with_excel_data():
+    excel = Files()
+    excel.open_workbook("SalesData.xlsx")
+    worksheet = excel.read_worksheet_as_table("data", header=True)
+    excel.close_workbook()
+
+    for row in worksheet:
+        fill_and_submit_sales_form(row)
